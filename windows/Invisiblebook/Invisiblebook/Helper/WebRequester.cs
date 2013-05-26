7c008b4b88dace6cd626b2f4413cd417babdcc28
Nguyễn Văn Được <nguyenvanduocit@gmail.com>
2013-05-26 18:14:43 +0700

    Add new lib

--- windows/Invisiblebook/Invisiblebook/Helper/WebRequester.cs
+++ windows/Invisiblebook/Invisiblebook/Helper/WebRequester.cs
@@ -4,53 +4,44 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
-
+using RestSharp;
+using Invisiblebook.Model;
 namespace Invisiblebook.Helper
 {
+    /// <summary>
+    /// Class hỗ trợ cho việc lấy thông tin từ server và tải các file
+    /// </summary>
     public class WebRequester
     {
-        string GET(string url) 
+        private RestClient _Client;
+        private string _BaseURL = "http://localhost/Invisiblebook";
+        private WebRequester _instance = null;
+        public WebRequester()
         {
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            try {
-                WebResponse response = request.GetResponse();
-                using (Stream responseStream = response.GetResponseStream()) {
-                    StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
-                    return reader.ReadToEnd();
-                }
-            }
-            catch (WebException ex) {
-                WebResponse errorResponse = ex.Response;
-                using (Stream responseStream = errorResponse.GetResponseStream())
-                {
-                    StreamReader reader = new StreamReader(responseStream, Encoding.GetEncoding("utf-8"));
-                    String errorText = reader.ReadToEnd();
-                    // log errorText
-                }
-                throw;
+            _Client = new RestClient(_BaseURL);
         }
-    }
-    // POST a JSON string
-    void POST(string url, string jsonContent) 
-    {
-        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-        request.Method = "POST";
-        request.ContentLength = jsonContent.Length;
-        request.ContentType = @"application/json";
-        System.Text.UTF8Encoding encoding = new System.Text.UTF8Encoding();
-        Byte[] byteArray = encoding.GetBytes(jsonContent);
+        /// <summary>
+        /// Dùng để lấy instance của đối tượng.
+        /// </summary>
+        /// <returns></returns>
+        public WebRequester GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new WebRequester();
+            }
+            return _instance;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public List<Book> GetLatestPostList()
+        {
+            List<Book> result = new List<Book>();
 
-        using (Stream dataStream = request.GetRequestStream()) {
-            dataStream.Write(byteArray, 0, byteArray.Length);
+            return result;
         }
-        long length = 0;
-        try {
-            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse()) {
-                length = response.ContentLength;
-            }
-        }
-        catch (WebException ex) {
-            // Log exception and throw as for GET example above
-        }
+
     }
 }
