367c5156bc152bf2b6a40d1096e8252f614a26f0
Nguyễn Văn Được <nguyenvanduocit@gmail.com>
2013-05-26 15:51:20 +0700

    Edit model

--- windows/Invisiblebook/Invisiblebook/Model/Chapter.cs
+++ windows/Invisiblebook/Invisiblebook/Model/Chapter.cs
@@ -7,5 +7,56 @@ namespace Invisiblebook.Model
 {
     public class Chapter
     {
+        #region private var
+        private int _ID;
+        private string _Name;
+        private Media _AudioName;
+        private Media _AudioContent; //Can be blank
+        #endregion
+
+        #region Constructor
+        public Chapter()
+        {
+            _ID = -1;
+        }
+        public Chapter(int id, string name, Media audioName, Media audioContent)
+        {
+            _ID = id;
+            _Name = name;
+            _AudioName = audioName;
+            _AudioContent = audioContent;
+        }
+        #endregion
+
+        #region Properties
+
+        public int ID
+        {
+            get { return _ID; }
+            set { _ID = value; }
+        }
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+
+        /// <summary>
+        /// Audio của tên chapter, không thể trống
+        /// </summary>
+        public Media AudioName
+        {
+            get { return _AudioName; }
+            set { _AudioName = value; }
+        }
+        /// <summary>
+        /// Audio Nội dung của chapter
+        /// </summary>
+        public Media AudioContent
+        {
+            get { return _AudioContent; }
+            set { _AudioContent = value; }
+        }
+        #endregion
     }
 }
--- windows/Invisiblebook/Invisiblebook/Model/Media.cs
+++ windows/Invisiblebook/Invisiblebook/Model/Media.cs
@@ -8,12 +8,58 @@ namespace Invisiblebook.Model
     public class Media
     {
         #region Private var
+        private int _ID;
         private string _FileName;
+        private string _FileURL;
         /// <summary>
         /// Người đọc
         /// </summary>
         private string _Announcer;
+        private DateTime _ModifyDate;
+        #endregion
 
+        #region constructor
+
+        public Media()
+        {
+            _ID = -1;
+        }
+        public Media(int id, string fileName, string fileURL, string announcer, DateTime modifyDate)
+        {
+            _ID = id;
+            _FileName = fileName;
+            _FileURL = fileURL;
+            _Announcer = announcer;
+            _ModifyDate = modifyDate;
+        }
+        #endregion
+
+        #region public propties
+        public int ID
+        {
+            get { return _ID; }
+            set { _ID = value; }
+        }
+        public string FileName
+        {
+            get { return _FileName; }
+            set { _FileName = value; }
+        }
+        public string FileURL 
+        {
+            get { return _FileURL; }
+            set { _FileURL = value; }
+        }
+        public string Announcer
+        {
+            get { return _Announcer; }
+            set { _Announcer = value; }
+        }
+        public DateTime ModifyDate
+        {
+            get { return _ModifyDate; }
+            set { _ModifyDate = value; }
+        }
         #endregion
     }
 }
